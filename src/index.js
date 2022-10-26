b553a28e3a53b5263444a12fd04b1fccbcc28e92
Agustin Sampayo <agustines82@gmail.com>
2022-10-26 16:44:49 -0300

    comitenando

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./database";
 //crear una instancia de express
 const app = express();
 //configurar un puerto
-app.set("port", process.env.PORT || 4000);
+app.set("port", process.env.PORT || 4001);
 
 app.listen(app.get("port"), () => {
     console.log("estoy en el puerto " + app.get("port"));
